9ec9e0a0c480f51c3d1f0ca815bdb6e205efe442
banagaurav <gauravbana840@gmail.com>
2024-11-10 00:41:39 +0545

    add Employee.cs model

--- Models/Department.cs
+++ Models/Department.cs
@@ -1,8 +1,17 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace RecordManagement.Models;
 
 public class Department
 {
-
+    public int EmployeeId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Gender { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Address { get; set; }
+    public bool IsActive { get; set; }
 }
