4522d002f3f24e86b1c4cdf894cff409f8eb1c97
banagaurav <gauravbana840@gmail.com>
2024-11-10 00:32:41 +0545

    create app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RecordManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
